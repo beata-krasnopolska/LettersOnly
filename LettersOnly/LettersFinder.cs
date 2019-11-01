8d13c41c5aa115a4b5ec4f6a6b6ca4d29a5e591d
beata.krasnopolska@gmail.com <beata.krasnopolska@gmail.com>
2019-11-01 10:41:03 +0100

    Add project files.

---
+++ LettersOnly/LettersFinder.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace LettersOnly
+{
+    class LettersFinder
+    {
+        public void Finder(string word)
+        {
+            var hiddenWord = new string(word.Where(char.IsLetter).ToArray());
+
+            Console.WriteLine("Word hidden in the ---   {0}   ----- is : {1}", word, hiddenWord);
+        }
+    }
+}
---
+++ LettersOnly/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LettersOnly
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program removes any non-letters from a string, returning a word");
+
+            string word1 = "R!=:~0o0./c&}9k`60=y";
+            string word2 = "^,]%4B|@56a![0{2m>b1&4i4";
+            string word3 = "^U)6$22>8p).";
+
+            var wordFinder = new LettersFinder();
+            wordFinder.Finder(word1);
+            wordFinder.Finder(word2);
+            wordFinder.Finder(word3);
+
+            Console.ReadKey();
+        }
+    }
+}
